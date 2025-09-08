aa351957d5bdcb7818f4bd779b7fbf12be769e5b
carellekuate <carellekuate5@gmail.com>
2025-09-08 15:39:55 +0100

    modication image

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,7 +2,8 @@
 import "./HomePageStyles.css";
 
 // Importation de l'image d'arrière-plan
-import mockupImage from "../../assets/images/mockup.jpg";
+import mockupImage from "../../assets/images/mockup.JPG";
+
 
 // Importation des images de cartes
 import card1 from "../../assets/images/fab.png";
