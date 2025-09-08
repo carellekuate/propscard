09a079cc5a45fe1f5830a1001d1bea9a6a4756bd
carellekuate <carellekuate5@gmail.com>
2025-09-08 14:27:35 +0100

    tout modifier entant des condition

--- src/pages/ExplorePage/ExplorePage.jsx
+++ src/pages/ExplorePage/ExplorePage.jsx
@@ -7,21 +7,66 @@ try {
 } catch (e) {
   img2 = '';
 }
-import img3 from '../../assets/images/fo.png';
-import img4 from '../../assets/images/fg.png';
-import img5 from '../../assets/images/fi.png';
-import img6 from '../../assets/images/ft.png';
-import img7 from '../../assets/images/fy.png';
-import img8 from '../../assets/images/fz.png';
-import img9 from '../../assets/images/fs.png';
+let img3;
+try {
+  img3 = require('../../assets/images/fo.png');
+} catch (e) {
+  img3 = '';
+}
+let img4;
+try {
+  img4 = require('../../assets/images/fg.png');
+} catch (e) {
+  img4 = '';
+}
+let img5;
+try {
+  img5 = require('../../assets/images/fi.png');
+} catch (e) {
+  img5 = '';
+}
+let img6;
+try {
+  img6 = require('../../assets/images/ft.png');
+} catch (e) {
+  img6 = '';
+}
+let img7;
+try {
+  img7 = require('../../assets/images/fy.png');
+} catch (e) {
+  img7 = '';
+}
+let img8;
+try {
+  img8 = require('../../assets/images/fz.png');
+} catch (e) {
+  img8 = '';
+}
+let img9;
+try {
+  img9 = require('../../assets/images/fs.png');
+} catch (e) {
+  img9 = '';
+}
 let img10;
 try {
   img10 = require('../../assets/images/fab.png');
 } catch (e) {
   img10 = '';
 } 
-import img11 from '../../assets/images/fe.png';
-import img12 from '../../assets/images/fk.png';    
+let img11;
+try {
+  img11 = require('../../assets/images/fe.png');
+} catch (e) {
+  img11 = '';
+}
+let img12;
+try {
+  img12 = require('../../assets/images/fk.png');
+} catch (e) {
+  img12 = '';
+}    
 import { navLinks } from '../../constants/NavLinks';
 import Navbar from '../../components/navbar/Navbar';
 import Footer from '../../components/footer/Footer';
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -22,21 +22,66 @@ try {
 } catch (e) {
   card2 = '';
 }
-import card3 from '../../assets/images/fi.png';
-import card4 from '../../assets/images/fj.png';
-import card5 from '../../assets/images/fa1.png';
-import card6 from '../../assets/images/fo.png';
-import card7 from '../../assets/images/ft.png';
-import card8 from '../../assets/images/fk.png';
+let card3;
+try {
+  card3 = require('../../assets/images/fi.png');
+} catch (e) {
+  card3 = '';
+}
+let card4;
+try {
+  card4 = require('../../assets/images/fj.png');
+} catch (e) {
+  card4 = '';
+}
+let card5;
+try {
+  card5 = require('../../assets/images/fa1.png');
+} catch (e) {
+  card5 = '';
+}
+let card6;
+try {
+  card6 = require('../../assets/images/fo.png');
+} catch (e) {
+  card6 = '';
+}
+let card7;
+try {
+  card7 = require('../../assets/images/ft.png');
+} catch (e) {
+  card7 = '';
+}
+let card8;
+try {
+  card8 = require('../../assets/images/fk.png');
+} catch (e) {
+  card8 = '';
+}
 let card9;
 try {
   card9 = require('../../assets/images/fa.png');
 } catch (e) {
   card9 = '';
 }
-import card10 from '../../assets/images/fs.png';
-import card11 from '../../assets/images/fe.png';
-import card12 from '../../assets/images/fg.png';
+let card10;
+try {
+  card10 = require('../../assets/images/fs.png');
+} catch (e) {
+  card10 = '';
+}
+let card11;
+try {
+  card11 = require('../../assets/images/fe.png');
+} catch (e) {
+  card11 = '';
+}
+let card12;
+try {
+  card12 = require('../../assets/images/fg.png');
+} catch (e) {
+  card12 = '';
+}
 
 // Importations des composantes
 import Footer from "../../components/footer/Footer";
@@ -55,7 +100,12 @@ try {
 } catch (e) {
   avatar2 = '';
 }
-import avatar3 from '../../assets/images/fi.png';
+let avatar3;
+try {
+  avatar3 = require('../../assets/images/fi.png');
+} catch (e) {
+  avatar3 = '';
+}
 
 // Importation des liens de navigation
 import { navLinks } from "../../constants/NavLinks";
