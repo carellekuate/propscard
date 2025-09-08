d77bb7308039aae6627bc26889f2f0763ac23e83
carellekuate <carellekuate5@gmail.com>
2025-09-08 13:48:24 +0100

    delete

--- src/pages/ExplorePage/ExplorePage.jsx
+++ src/pages/ExplorePage/ExplorePage.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
 import './ExplorePageStyles.css';
 import { FaBeer } from 'react-icons/fa';
-import img1 from '../../assets/images/fa.png';
+let img1;
+try {
+  img1 = require('../../assets/images/fa.png');
+} catch (e) {
+  img1 = '';
+}
 import img2 from '../../assets/images/fb1.png';
 import img3 from '../../assets/images/fo.png';
 import img4 from '../../assets/images/fg.png';
